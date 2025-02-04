aae2263e1e141de95d880b093da9844b6673fbea
gerard.moscardo <gerard.moscardo@edreamsodigeo.com>
2025-02-04 11:00:26 +0100

    Added amount od cache news per rss configuration in control panel

--- db/mongoWrapper.js
+++ db/mongoWrapper.js
@@ -52,6 +52,7 @@ exports.setRss = async function (rss) {
                     url: rss.url,        // Usa rss.url para el campo url
                     category: rss.category, // Usa rss.category para el campo category
                     isActive: rss.isActive,
+                    maxElementsCache: parseInt(rss.maxElementsCache)
                 },
             },
             { upsert: true, setDefaultsOnInsert: true }
