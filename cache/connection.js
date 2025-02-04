4770d4d3490d3aec24a31ff97f0f75ba3c375df4
PC\a <a>
2025-02-04 15:48:15 +0100

    Styles4AdvancedSearch

--- cache/connection.js
+++ cache/connection.js
@@ -1,6 +1,6 @@
 const redis = require('redis'); // Importar Redis
 const redisClient = redis.createClient({
-    url: 'redis://host.docker.internal:6379', // Cambiar si tienes otra configuración de Redis
+    url: 'redis://172.27.160.1:6379', // Cambiar si tienes otra configuración de Redis
 });
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 redisClient.connect()
